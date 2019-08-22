1859f0dd91e59339c2584fbad9798431eb1af5d1
J-Vo <jordanvolkman@gmail.com>
2019-08-21 20:52:55 -0500

    Initial Commit
    All the changes. EVERYTHING.

---
+++ Assets/Scripts/GunScript.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class GunScript : MonoBehaviour
+{
+    public float damage = 10f;
+    public float bulletSpeed = 1;
+
+
+    public Camera fpsCam;
+    public GameObject bullet;
+    public ParticleSystem muzzleflash;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+
+    }
+
+    void Shoot()
+    {
+        GameObject tempBullet = Instantiate(bullet, transform.position, transform.rotation) as GameObject;
+        Rigidbody tempRigidBodyBullet = tempBullet.GetComponent<Rigidbody>();
+        tempRigidBodyBullet.AddForce(tempRigidBodyBullet.transform.forward * bulletSpeed);
+        Destroy(tempBullet, 0.5f);
+       
+    }
+}
